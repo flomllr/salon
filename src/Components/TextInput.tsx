9acba8c04f84f865024d5b57934ae70614fa68ad
Justin Glibert <justin@glibert.io>
2020-04-12 13:26:22 +0200

    Welcome + UI kit

---
+++ src/Components/TextInput.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+background-color: transparent;
+color: #69686A;
+outline: none;
+outline-style: none;
+outline-offset: 0;
+border-top: none;
+border-left: none;
+border-right: none;
+border-bottom: solid #eeeeee 1px;
+font-family: 'Raleway', sans-serif;
+font-size: 14px;
+padding: 3px 10px;
+margin: 10px 0px 10px 0px;
+`;
---
+++ src/__jsxfixtures__/Signup.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import Signup from "../Components/Signup";
+import '../index.css'
+export default <Signup join={() => {}} setData={(data) => {}} />;
